7c5d895b7ee07ebb21066228f1cf0408d341ace9
architokxD <104564828+architokxD@users.noreply.github.com>
2022-06-18 22:29:51 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -179,12 +179,6 @@ SONG_DOWNLOAD_DURATION_LIMIT = int(
     time_to_seconds(f"{SONG_DOWNLOAD_DURATION}:00")
 )
 
-if UPSTREAM_REPO:
-    if not re.match("(?:http|https)://", UPSTREAM_REPO):
-        print(
-            "[ERROR] - Your UPSTREAM_REPO url is wrong. Please ensure that it starts with https://"
-        )
-        sys.exit()
 
 if PING_IMG_URL:
     if PING_IMG_URL != "assets/Ping.jpeg":
