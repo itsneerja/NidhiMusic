10fb4104cf174ac60e290f4aadf2ab5835f6c6a0
movieverseofc <103868734+movieverseofc@users.noreply.github.com>
2022-07-03 17:08:51 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -55,7 +55,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                    text="💘sᴏᴜʀᴄᴇ💘", url=f"{config.UPSTREAM_REPO}"
+                    text="💘sᴏᴜʀᴄᴇ💘", url=f"{config.GITHUB_REPO}"
                 )
         ],
      ]
