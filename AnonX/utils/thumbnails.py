a3667484e03bc0886126b6bd843050a3ab6f6885
movieverseofc <103868734+movieverseofc@users.noreply.github.com>
2022-07-03 17:00:06 +0530

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -126,8 +126,8 @@ async def gen_thumb(videoid):
             font4 = ImageFont.truetype('assets/font2.ttf', 35)
 
             image4 = ImageDraw.Draw(image2)
-            image4.text((10, 10), "FALLEN MUSIC", fill="white", font = font1, align ="left") 
-            image4.text((670, 150), "NOW PLAYING", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
+            image4.text((10, 10), "ɴɪᴅʜɪ ᴍᴜsɪᴄ", fill="white", font = font1, align ="left") 
+            image4.text((670, 150), "1sʜᴏᴛ-ɴᴇᴇʀᴊᴀ", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
 
             # title
             title1 = truncate(title)
