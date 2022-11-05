49e691882a325c6237f77dc4eb50f6341f9136fc
movieverseofc <103868734+movieverseofc@users.noreply.github.com>
2022-11-06 00:19:26 +0530

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -126,8 +126,8 @@ async def gen_thumb(videoid):
             font4 = ImageFont.truetype('assets/font2.ttf', 35)
 
             image4 = ImageDraw.Draw(image2)
-            image4.text((10, 10), "NIDHI BABES", fill="white", font = font1, align ="left") 
-            image4.text((670, 150), "1SHOT-NEERJA", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
+            image4.text((10, 10), "NIDHI MUSIC", fill="white", font = font1, align ="left") 
+            image4.text((670, 150), "•NEERJA•", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
 
             # title
             title1 = truncate(title)
